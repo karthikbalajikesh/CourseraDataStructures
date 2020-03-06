8227ad7d5366ea1da4ab0d5eed43fda65b62f8e9
Karthik Keshavamurthi <karthikbalaji@vt.edu>
2020-03-06 18:16:02 -0500

    added euclidian  gcf

--- GCD/gcd.cpp
+++ GCD/gcd.cpp
@@ -4,18 +4,19 @@
 
 using namespace std;
 
-long long gcd_naive(long long n1, long long n2);
+long long int gcd_naive(long long int n1, long long int n2);
+long long int gcd_euclidian(long long int n1, long long int n2);
 int main() {
-	long long n1 = 1, n2 = 1;
+	long long int n1 = 1, n2 = 1;
 	cout << "Enter the two Numbers: \t";
 	cin >> n1 >> n2;
-	cout << gcd_naive(n1, n2);
+	cout << gcd_euclidian(n1, n2);
 
 	return 0;
 }
 
-long long gcd_naive(long long n1, long long n2) {
-	long long gcd = 0;
+long long int gcd_naive(long long int n1, long long int n2) {
+	long long int gcd = 0;
 	// Algorithm, we are going to compare the two numbers and find the factors of the smaller number.
 	// Before that, we check if the number itself is a factor. then it will be the gcd
 	if (n1 < 1 || n2 < 1) {
@@ -27,12 +28,12 @@ long long gcd_naive(long long n1, long long n2) {
 		return gcd;
 	}
 	// Now we will iterate through the larger number
-	long long temp = (n1> n2) ? n1 : n2;
-	long long temp2 = (n1 < n2) ? n1 : n2;
+	long long int temp = (n1> n2) ? n1 : n2;
+	long long int temp2 = (n1 < n2) ? n1 : n2;
 
 	
 	// we will iterate and store the factors of the larger value
-	for (long long i =sqrt(temp2); i > 0; i--) {
+	for (long long int i =sqrt(temp2); i > 0; i--) {
 		if ((temp % i == 0 )&&(temp2%i==0)) {
 			gcd = i;
 			return i;
@@ -41,4 +42,17 @@ long long gcd_naive(long long n1, long long n2) {
 
 
 	return gcd;
+}
+
+long long int gcd_euclidian(long long int n1, long long int n2) {
+
+	if (n2 == 0){
+		return n1;
+
+	}
+	else {
+		return (n2, n1 % n2);
+
+	}
+
 }
